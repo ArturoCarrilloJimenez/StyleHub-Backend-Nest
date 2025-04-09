0ef1afd086401318c66065131082e1a95a632342
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-04-09 13:13:32 +0200

    prueba de reventar el programa

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactorydadaedaeqw);
 
   await app.listen(process.env.PORT ?? 3000);
 }
